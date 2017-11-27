0fc2664f7f1cffb90fcc52494307c0650f03bbb6
Alex Barnett <alexbarnett@AlexBarnett-MacBook.local>
2017-11-27 15:33:02 +0000

    added links for books

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+// GET list of all books
+router.get('/', (req, res, next) => {
+    res.render('books/all', { title: 'Books' });
+});
+
 // GET new book page
 router.get('/new', (req, res, next) => {
     res.render('books/new', { title: 'New Book' });
+});
+
+// GET list of overdue books
+router.get('/overdue', (req, res, next) => {
+    res.render('books/overdue', { title: 'Overdue Books' });
+});
+
+// GET list of checked out books
+router.get('/checked', (req, res, next) => {
+    res.render('books/checked', { title: 'Checked Out Books' });
 });
 
 module.exports = router;
